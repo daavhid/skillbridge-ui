5592e385b22bcbd637716ed460c1d4e2dd6f2700
daavhid <victortoyosi06@gmail.com>
2024-03-01 01:43:17 +0100

    navbar and the Hero Section

---
+++ Home/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './**/*.html', // Include all HTML files in the project directory and its subdirectories
+        './**/*.js', 
+      ],
+  theme: {
+    extend: {
+        fontFamily:{
+            rubik:['rubiks']
+        },
+        colors:{
+            yellow: 'rgba(255, 214, 0, 1)'
+
+        }
+    },
+  },
+  plugins: [],
+}
+
