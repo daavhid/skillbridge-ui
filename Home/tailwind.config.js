24cdefeab2fd5c2f3d62d7ef77d7991b062233ea
daavhid <victortoyosi06@gmail.com>
2024-03-06 19:00:11 +0100

    finished the home

--- Home/tailwind.config.js
+++ Home/tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         colors:{
             yellow: 'rgba(255, 214, 0, 1)',
             color2:' rgba(1, 39, 55, 1)'
+            
         },
         backgroundImage:(theme)=>({
             gradient1: 'linear-gradient(270deg, rgba(1, 39, 55, 1) 0%, #0D4D69 100%);'  
